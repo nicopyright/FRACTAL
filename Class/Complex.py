f6a3046b7539d8ec1039f1618555993c51ce58e0
nicopyright <nicolas.metais71@gmail.com>
2023-05-10 17:14:13 +0200

    Complex Class Complete

--- Class/Complex.py
+++ Class/Complex.py
@@ -1,2 +1,48 @@
+# Complex class file.
+# we need to use complexes for the Julia set of fractals
+class Complex:
+    def __init__(self,
+                 r: float = 0.0,
+                 i: float = 0.0) -> None:
+        """
+        :param r: reel part of the complex
+        :param i: imaginary part of the complex
+        """
+        self.re = float(r)
+        self.im = float(i)
 
+    def __repr__(self) -> str:
+        """
+        :return: "Re(z) + i*Im(z)"
+        """
+        return "{} +i{}".format(self.re, self.im)
 
+    def __add__(self, other) -> "Complex":
+        """
+        :return: addition of two complexes result
+        """
+        return Complex(self.re + other.re, self.im + other.im)
+
+    def __sub__(self, other) -> "Complex":
+        """
+        :return: subtraction of two complexes result
+        """
+        return Complex(self.re - other.re, self.im - other.im)
+
+    def __mul__(self, other) -> "Complex":
+        """
+        :return: multiplication of two complexes result
+        """
+        return Complex(self.re * other.re - self.im * other.im, self.re * other.im + other.re * self.im)
+
+    def __abs__(self) -> float:
+        """
+        :return: Modulus of the complex z
+        """
+        return (self.re ** 2 + self.im ** 2) ** (1 / 2)
+
+    def coord(self) -> tuple:
+        """
+        :return: Coordinates with a Tuple
+        """
+        return self.re, self.im
