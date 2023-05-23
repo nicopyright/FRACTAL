8bd0b78eebd323dcb2d1f6d15505604a0dba0c4c
Pumch <pumch999@hotmail.fr>
2023-05-23 10:31:48 +0200

    Bug correction

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import pygame
 from multiprocessing import Process, cpu_count
 import time
 import threading
-from decimal import Decimal
 
 pygame.init()
+
+
 
 # collect screen's informations
 v_screen_size = pygame.display.Info()
@@ -19,11 +20,11 @@ v_screen_height = v_screen_size.current_h
 v_center = (v_screen_width // 2, v_screen_height // 2)
 
 
-v_targ = "julia"
+v_widtharg = "julia"
 
 v_w = Complex(-1, 0)
-v_width = 400
-v_height = 400
+v_height = v_screen_height
+v_width = v_screen_width
 v_zoom = 100
 v_spreadX = cpu_count() * 2
 v_spreadY = cpu_count() * 2
@@ -31,7 +32,7 @@ v_spreadY = cpu_count() * 2
 
 if __name__ == "__main__":
 
-    v_window = pygame.display.set_mode((v_screen_width // 2, v_screen_height // 2))
+    v_window = pygame.display.set_mode((v_screen_width // 2, v_screen_height // 2), pygame.RESIZABLE)
     v_window.fill("white")
 
     v_cont = True
@@ -41,27 +42,26 @@ if __name__ == "__main__":
     v_k = 0
 
     start = time.time()
-    if v_targ == "julia":
-        v_ths = [[threading.Thread(target=julia, args=(
-            v_window, i * (v_height // v_spreadX), j * (v_width // v_spreadY),
-            (i + 1) * (v_height // v_spreadX), (j + 1) * (v_width // v_spreadY), v_w, 0, 0,
+    if v_widtharg == "julia":
+        v_widthhs = [[threading.Thread(target=julia, args=(
+            v_window, i * (v_width // v_spreadX), j * (v_height // v_spreadY),
+            (i + 1) * (v_width // v_spreadX), (j + 1) * (v_height // v_spreadY), v_w, 0, 0,
             v_zoom)) for i in range(-v_spreadX // 2, v_spreadX // 2)] for j in
                  range(-v_spreadY // 2, v_spreadY // 2)]
 
-    elif v_targ == "mandelbrot":
-        v_ths = [[threading.Thread(target=mandelbrot, args=(
-            v_window, i * (v_width // v_spreadX), j * (v_height // v_spreadY), (i + 1) * (v_width // v_spreadX),
-            (j + 1) * (v_height // v_spreadY), 0, 0,
+    elif v_widtharg == "mandelbrot":
+        v_widthhs = [[threading.Thread(target=mandelbrot, args=(
+            v_window, i * (v_height // v_spreadX), j * (v_width // v_spreadY), (i + 1) * (v_height // v_spreadX),
+            (j + 1) * (v_width // v_spreadY), 0, 0,
             v_zoom)) for i in range(-v_spreadX // 2, v_spreadX // 2)] for j in range(-v_spreadY // 2, v_spreadY // 2)]
 
-    for thx in v_ths:
+    for thx in v_widthhs:
         for th in thx:
             th.start()
 
-    for thx in v_ths:
+    for thx in v_widthhs:
         for th in thx:
             th.join()
-
 
     end = time.time()
     print(end-start)
@@ -86,25 +86,25 @@ if __name__ == "__main__":
                 v_window.fill("white")
                 v_zoom *= 2
 
-                if v_targ == "julia":
-                    v_ths = [[threading.Thread(target=julia, args=(
-                        v_window, i * (v_height // v_spreadX), j * (v_width // v_spreadY), (i + 1) * (v_height // v_spreadX),
-                        (j + 1) * (v_height // v_spreadY), v_w,
+                if v_widtharg == "julia":
+                    v_widthhs = [[threading.Thread(target=julia, args=(
+                        v_window, i * (v_width // v_spreadX), j * (v_height // v_spreadY), (i + 1) * (v_width // v_spreadX),
+                        (j + 1) * (v_width // v_spreadY), v_w,
                         t_mouseX[v_k], t_mouseY[v_k], v_zoom)) for i in range(-v_spreadX // 2, v_spreadX // 2)] for j in
                            range(-v_spreadY // 2, v_spreadY // 2)]
 
-                elif v_targ == "mandelbrot":
-                    v_ths = [[threading.Thread(target=mandelbrot, args=(
-                        v_window, i * (v_width // v_spreadX), j * (v_height // v_spreadY), (i + 1) * (v_width // v_spreadX),
-                        (j + 1) * (v_height // v_spreadY), t_mouseX[v_k], t_mouseY[v_k],
+                elif v_widtharg == "mandelbrot":
+                    v_widthhs = [[threading.Thread(target=mandelbrot, args=(
+                        v_window, i * (v_height // v_spreadX), j * (v_width // v_spreadY), (i + 1) * (v_height // v_spreadX),
+                        (j + 1) * (v_width // v_spreadY), t_mouseX[v_k], t_mouseY[v_k],
                         v_zoom)) for i in range(-v_spreadX // 2, v_spreadX // 2)] for j in range(-v_spreadY // 2, v_spreadY // 2)]
 
 
-                for thx in v_ths:
+                for thx in v_widthhs:
                     for th in thx:
                         th.start()
 
-                for thx in v_ths:
+                for thx in v_widthhs:
                     for th in thx:
                         th.join()
 
